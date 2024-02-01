ef0099d8e37ddebc7200c616b585c498594e4add
Matheus-uzk <uzk11@hotmail.com>
2024-02-01 19:21:49 -0300

    Meu segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log ("ola mundo")
+let xpHeroi = 13.500
+let nomeHeroi = "Bangalore"
+let eloHeroi = ""
+
+if (xpHeroi >= 10.001){
+eloHeroi = "Radiante,"
+
+}
+
+console.log ("O Herói de nome " + nomeHeroi + " está no nível de: " + eloHeroi + " com " + xpHeroi + " de XP")
